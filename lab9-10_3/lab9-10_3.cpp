0d3b8102b0e3f753c8ec94853f9cc322be8c4f9d
Никита <@DESKTOP-60LM23F>
2023-05-05 00:54:55 +0700

    Добавьте файлы проекта.

---
+++ lab9-10_3/lab9-10_3.cpp
@@ -0,0 +1,24 @@
+﻿#include "Header.h"
+
+void main()
+{
+    // Создать единственный экземпляр класса Singleton
+    Singleton* obj1 = Singleton::Instance();
+
+    if (obj1 != nullptr)
+    {
+        obj1->Set(255);
+        obj1->Print(); // d = 255
+    }
+
+    // Попытка создания другого экземляра класса Singleton
+    Singleton* obj2 = Singleton::Instance(); 
+
+    if (obj2 != nullptr)
+    {
+        obj2->Set(300);
+        obj2->Print();
+    }
+
+    delete obj1, delete obj2;
+}
